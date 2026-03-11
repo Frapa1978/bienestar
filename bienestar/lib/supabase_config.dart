07001b44827ab625ba872f67cede00940ce668e7
Frapa1978 <francisco.pachecob@gmail.com>
2026-03-11 16:32:14 -0500

    lanzamiento bienestar

---
+++ bienestar/lib/supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String url = 'https://nbkkbxxilhsucgaemncd.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ia2tieHhpbGhzdWNnYWVtbmNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0ODIzNDEsImV4cCI6MjA4ODA1ODM0MX0.T-WT2O6BgWXL_lJdwibXiEkHIi-ZSZis9vyt-vfnX8Q';
+}
